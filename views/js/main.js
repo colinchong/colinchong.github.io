1638ed3e22ca790838760192878a8d8ca94b8ffc
Colin Chong <colinchong88@gmail.com>
2017-05-14 02:15:11 -0600

    Fixed comments, added back in determineDX function

--- views/js/main.js
+++ views/js/main.js
@@ -420,6 +420,12 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
+   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
+  function determineDx (elem, size) {
+    var oldWidth = elem.offsetWidth;
+    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var oldSize = oldWidth / windowWidth;
+
     // Changes the slider value to a percent width
     function sizeSwitcher (size) {
       switch(size) {
