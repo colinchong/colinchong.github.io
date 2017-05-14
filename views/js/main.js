14e124d0690f708fba1c49229104f34b6f0c216e
Colin Chong <colinchong88@gmail.com>
2017-05-13 19:07:20 -0600

    More flexible calculation for pizza DOM loading

--- views/js/main.js
+++ views/js/main.js
@@ -537,7 +537,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 20; i++) {
+  var rows = Math.ceil(window.innerHeight / s) + 1;
+  for (var i = 0; i < cols * rows; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
